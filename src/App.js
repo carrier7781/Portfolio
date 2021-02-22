29dd11be34580f1318c6749404177670a23b101c
Korey Carrier <carrier7781@gmail.com>
2021-02-21 22:35:34 -0500

    header somewhat complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles-js";
 import Navbar from "./components/Navbar.js";
+import Header from "./components/Header";
+
 
 
 function App() {
   return (
+    <>
+      <Particles 
+      className="particles-canvas"
+        params= {{
+          particles: {
+          number: {
+            value: 30,
+            density: {
+              enable: true,
+              value_area: 900
+            } 
+          },
+          shape: {
+            type: "circle",
+            stroke: {
+              width: 6,
+              color: "f9ab00"
+            }
+          }
+        }
+        }}
+      
+      />
       <Navbar />
+      <Header />
+
+  </>
   );
 }
 
